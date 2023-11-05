449d1e7dc2a21f04f80015e0c43cda4df05cfe67
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-05 21:40:33 +0530

    8 completed

---
+++ 08 Constent manupulation/code.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    // int a = 34;
+    // a = 45;
+    // cout << "The Value of A is Now: " << a << endl;
+
+    // ---------Constants in c++ ---------------
+    // const int a = 34;
+    // cout << "The Value of A was : " << a << endl;
+    // a = 45;
+    // cout << "The Value of A is Now: " << a << endl;
+
+    //  ************** Manipulators in C++ *******************
+
+    // int a = 4, b = 10, c = 423, d = 4561, e = 45612;
+
+    // cout << "The Value of a is without setw is:" << a << endl;
+    // cout << "The Value of b is without setw is:" << b << endl;
+    // cout << "The Value of c is without setw is:" << c << endl;
+    // cout << "The Value of d is without setw is:" << d << endl;
+    // cout << "The Value of e is without setw is:" << e << endl;
+
+    // cout << endl;
+    // cout << endl;
+
+    // cout << "The Value of a is with setw is:" << setw(5) << a << endl;
+    // cout << "The Value of b is with setw is:" << setw(5) << b << endl;
+    // cout << "The Value of c is with setw is:" << setw(5) << c << endl;
+    // cout << "The Value of d is with setw is:" << setw(5) << d << endl;
+    // cout << "The Value of e is with setw is:" << setw(5) << e << endl;
+
+    // *************** Operater Precedence **************
+    // Operator precedence in C++ determines the order in which different operators are evaluated in an expression. Here is a list of some common C++ operators in order of their precedence, from highest to lowest, along with their associativity:
+
+    // 1. Postfix operators (left to right):
+    //    - `()` - Function call
+    //    - `[]` - Array subscript
+    //    - `.` - Member access (object.member)
+    //    - `->` - Member access through pointer (objectPtr->member)
+
+    // 2. Unary operators (right to left):
+    //    - `++` - Post-increment (e.g., `x++`)
+    //    - `--` - Post-decrement (e.g., `x--`)
+    //    - `+` - Unary plus
+    //    - `-` - Unary minus
+    //    - `!` - Logical NOT
+    //    - `~` - Bitwise NOT
+    //    - `&` - Address-of
+    //    - `*` - Dereference
+
+    // 3. Multiplicative operators (left to right):
+    //    - `*` - Multiplication
+    //    - `/` - Division
+    //    - `%` - Modulus
+
+    // 4. Additive operators (left to right):
+    //    - `+` - Addition
+    //    - `-` - Subtraction
+
+    // 5. Shift operators (left to right):
+    //    - `<<` - Left shift
+    //    - `>>` - Right shift
+
+    // 6. Relational operators (left to right):
+    //    - `<` - Less than
+    //    - `>` - Greater than
+    //    - `<=` - Less than or equal to
+    //    - `>=` - Greater than or equal to
+
+    // 7. Equality operators (left to right):
+    //    - `==` - Equal to
+    //    - `!=` - Not equal to
+
+    // 8. Bitwise AND (left to right):
+    //    - `&` - Bitwise AND
+
+    // 9. Bitwise XOR (left to right):
+    //    - `^` - Bitwise XOR
+
+    // 10. Bitwise OR (left to right):
+    //     - `|` - Bitwise OR
+
+    // 11. Logical AND (left to right):
+    //     - `&&` - Logical AND
+
+    // 12. Logical OR (left to right):
+    //     - `||` - Logical OR
+
+    // 13. Conditional operator (right to left):
+    //     - `? :` - Ternary conditional operator (e.g., `condition ? true_expression : false_expression`)
+
+    // 14. Assignment operators (right to left):
+    //     - `=` - Assignment
+    //     - `+=` - Addition assignment
+    //     - `-=` - Subtraction assignment
+    //     - `*=` - Multiplication assignment
+    //     - `/=` - Division assignment
+    //     - `%=` - Modulus assignment
+    //     - `&=` - Bitwise AND assignment
+    //     - `|=` - Bitwise OR assignment
+    //     - `^=` - Bitwise XOR assignment
+    //     - `<<=` - Left shift assignment
+    //     - `>>=` - Right shift assignment
+
+    // 15. Comma operator (left to right):
+    //     - `,` - Comma operator
+
+    return 0;
+}
