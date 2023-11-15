cc74ada3618c988d8fc6e48c4e031277bda5ab8d
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-15 06:19:34 +0530

    24 revised

---
+++ 24 Static data member/02code.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class employess
+{
+    int id;
+    static int countert;
+
+public:
+    void setData(void)
+    {
+        cout << "Enter the id : " << endl;
+        cin >> id;
+        countert++;
+    };
+
+    void getData(void)
+    {
+        cout << "The Id of This Employee is : " << id << " This is Employee No . " << countert << endl;
+    }
+
+    static void getcount(void)
+    {
+        cout << "The value of count is : " << countert << endl;
+    }
+};
+
+// int employess::countert; // default value is 0.
+int employess::countert = 1000; // but i can gives a initial value here.
+int main()
+{
+    // employess gaurav;
+    // employess saurav;
+    // employess pinku;
+
+    employess gaurav, saurav, pinku;
+
+    gaurav.setData();
+    gaurav.getData();
+    employess::getcount();
+
+    saurav.setData();
+    saurav.getData();
+    employess::getcount();
+
+    pinku.setData();
+    pinku.getData();
+    employess::getcount();
+
+    return 0;
+}
