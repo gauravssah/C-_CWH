95e52858581ceaafa8ca036b2a8de54e6700b56f
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-04 06:31:39 +0530

    revise recursion

--- 18 Recursions recursive function/recode.cpp
+++ 18 Recursions recursive function/recode.cpp
@@ -1,15 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// --------------------------->
+
 int calFactorial(int n)
 {
     if (n <= 1)
     {
         return 1;
     }
-
     return n * calFactorial(n - 1);
 }
+
+// --------------------------->
+
+int fib(int n)
+{
+    if (n < 2)
+    {
+        return 1;
+    }
+
+    return fib(n - 2) + fib(n - 1);
+}
+
+// --------------------------->
 
 int main()
 {
@@ -17,7 +32,9 @@ int main()
     cout << "Enter The Number." << endl;
     cin >> num;
 
-    cout << "The factorial Of " << num << " is " << calFactorial(num) << endl;
+    // cout << "The factorial Of " << num << " is " << calFactorial(num) << endl;
+
+    // cout << "The fib num at " << num << " is " << fib(num) << endl;
 
     return 0;
 }
