462b094c50e8e941e6969285c83ccde9643412b0
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-06 22:29:40 +0530

    complex num

---
+++ 25 Array of Objects/arrayEmply.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Employee
+{
+private:
+    int id;
+    int salery;
+
+public:
+    void setData(void)
+    {
+        salery = 512;
+        cout << "Enter The Id: " << endl;
+        cin >> id;
+    }
+
+    void getData(void)
+    {
+        cout << "The id of This Employee is : " << id << endl;
+        cout << salery << endl;
+    }
+};
+
+int main()
+{
+
+    Employee meta[4];
+
+    for (int i = 0; i < 4; i++)
+    {
+        meta[i].setData();
+        meta[i].getData();
+    }
+
+    return 0;
+}
