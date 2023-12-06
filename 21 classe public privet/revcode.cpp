d42afa1a514845da9d8d50d549b6c5f817cb7d26
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-06 15:37:24 +0530

    class, private and public

---
+++ 21 classe public privet/revcode.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Emply
+{
+private:
+    int a, b, c;
+
+public:
+    int d, e;
+
+    void setData(int x, int y, int z); // Only function declaration
+    // void getData()    // Here declear and define
+    // {
+    //     cout << "The value of a is: " << a << endl;
+    //     cout << "The value of b is: " << b << endl;
+    //     cout << "The value of c is: " << c << endl;
+    //     cout << "The value of d is: " << d << endl;
+    //     cout << "The value of e is: " << e << endl;
+    // }
+
+    void getData(); // Only function declaration
+};
+
+void Emply ::setData(int p, int q, int r)
+{
+    a = p;
+    b = q;
+    c = r;
+}
+
+void Emply ::getData()
+{
+    cout << "The value of a is: " << a << endl;
+    cout << "The value of b is: " << b << endl;
+    cout << "The value of c is: " << c << endl;
+    cout << "The value of d is: " << d << endl;
+    cout << "The value of e is: " << e << endl;
+}
+
+int main()
+{
+    Emply gaurav;
+    gaurav.setData(1, 2, 3);
+    gaurav.d = 11;
+    gaurav.e = 12;
+
+    gaurav.getData();
+    return 0;
+}
